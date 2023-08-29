12bff32eca4adee09e7f89266a3077d7d853a357
ImOmPandya <ompandya2509@gmail.com>
2023-08-29 22:24:06 +0530

    Initial commit

---
+++ test.js
@@ -0,0 +1,16 @@
+const { executeCommands } = require('./solution'); 
+const { describe, test, expect } = require('jest');
+
+describe('Chandrayaan 3 Lunar Craft', () => {
+  test('should move the spacecraft forward', () => {
+    const initialPosition = [0, 0, 0];
+    const initialDirection = 'N';
+    const commands = ['f'];
+
+    const finalState = executeCommands(initialPosition, initialDirection, commands);
+
+    expect(finalState.position).toEqual([0, 1, 0]);
+    expect(finalState.direction).toBe('N');
+  });
+
+});
